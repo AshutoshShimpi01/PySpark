2873c8a7df7da4bad47a9a23fbbce2dbf47075a2
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2025-10-19 22:44:34 +0530

    Update rank.py

--- Basics/WINDOWS/rank.py
+++ Basics/WINDOWS/rank.py
@@ -4,3 +4,16 @@ from pyspark.sql.functions import *
 window_spec = Window.partitionBy('dept_id').orderBy(col('salary').desc())
 
 employees_df.withColumn('rk',rank().over(window_spec)).show()
+
+
+
+
+
+Dense_rank
+
+from pyspark.sql.window import Window
+from pyspark.sql.functions import *
+
+window_spec_group_total = Window.partitionBy('dept_id').orderBy(col('salary').desc())
+
+employees_df.withColumn('d_rk', dense_rank().over(window_spec_group_total)).show()
