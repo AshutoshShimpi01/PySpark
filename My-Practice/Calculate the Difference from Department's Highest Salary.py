f1c17a59b7cfe60f557e93f4b63f148660562e95
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2025-12-11 00:07:11 +0530

    Update Calculate the Difference from Department's Highest Salary.py

--- My-Practice/Calculate the Difference from Department's Highest Salary.py
+++ My-Practice/Calculate the Difference from Department's Highest Salary.py
@@ -1,6 +1,6 @@
 
-
-
+PERFECT
+---------
 ####For each employee, calculate the **difference** between their salary and the highest salary within their department.
 Display the employee name, salary, and the difference. (Hint: Use `max` as a window function).
 
@@ -11,3 +11,31 @@ wind = Window.partitionBy('dept_id')
 check = jn_df.withColumn('new', max('salary').over(wind) - col('salary'))
 
 check.select('name','salary','new').show()
+
+
+
+
+
+
+
+
+
+
+
+SAME
+
+
+
+
+from pyspark.sql.functions import sum, col
+from pyspark.sql.window import Window
+
+join_df = employees_df.join(departments_df, 'dept_id')
+
+####For each employee, calculate the **difference** between their salary and the highest salary within their department.
+# Display the employee name, salary, and the difference. (Hint: Use `max` as a window function).
+
+win = Window.partitionBy('dept_id').orderBy(col('salary').desc())
+
+join_df.withColumn('max_sal', max(col('salary')).over(win)).withColumn('difference', col('max_sal') - col('salary')).show()
+
