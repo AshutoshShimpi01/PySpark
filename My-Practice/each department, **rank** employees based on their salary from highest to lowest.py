b5dfef7248b42f2beb4973e927f284800ca79f37
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2025-12-11 15:36:26 +0530

    Update and rename Rank the Employees by Salary.py to each department, **rank** employees based on their salary from highest to lowest.py

--- My-Practice/each department, **rank** employees based on their salary from highest to lowest.py
+++ My-Practice/each department, **rank** employees based on their salary from highest to lowest.py
@@ -3,18 +3,16 @@
 Display the employee name, salary, department name, and the salary rank. Use the `rank` function.
 
 
-
-from pyspark.sql.window import Window
-from pyspark.sql.functions import *
-
-
 ####For each department, **rank** employees based on their salary from highest to lowest. Display the employee name, salary, department name, and the salary rank. Use the `rank` function.
+
+from pyspark.sql.functions import *
+from pyspark.sql.window import *
+
 
 jn_df = employees_df.join(departments_df, 'dept_id')
 
 win = Window.partitionBy('dept_id').orderBy(col('salary').desc())
 
-final_df = jn_df.withColumn('rk', rank().over(win))
+final_df = jn_df.withColumn('rk', rank().over(win)).select('name','salary','dept_name','rk').show()
 
-final_df.select('name','salary','dept_name','rk').show()
 
