f0c6f98e081d83da5c788023df17ebbca2b6f473
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2025-12-11 15:32:02 +0530

    Update Find Empolyees which salary > then DEPT AVERAGE salary.py

--- My-Practice/Find Empolyees which salary > then DEPT AVERAGE salary.py
+++ My-Practice/Find Empolyees which salary > then DEPT AVERAGE salary.py
@@ -13,3 +13,32 @@ jn_df = employees_df.join(dp_av, on='dept_id', how='inner')
 
 # 3. Filter employees whose salary is greater than the department average
 jn_df.filter(col('salary') > col('dept_avg')).select('emp_id', 'dept_id', 'salary', 'dept_avg').show()
+
+
+
+
+SAME USING WINDOW FUNCTIONS
+
+
+steps -  
+join datafraes --> write window --> create new dataframe to perform aggregration --> apply finter condition inside question
+
+
+jn = employees_df.join(departments_df, 'dept_id')
+win = Window.partitionBy('dept_id').orderBy(col('salary'))
+check = jn.withColumn('dept_av', avg('salary').over(win))
+check.filter(col('salary') > col('dept_av')).show()
+
+
+
+
+
+
+
+
+
+
+
+.
+
+
