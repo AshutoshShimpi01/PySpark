8291a0ec517c4b4f11a334402c72df58affd05e9
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2025-12-14 12:04:56 +0530

    Update Find Empolyees which salary > then DEPT AVERAGE salary.py

--- My-Practice/Find Empolyees which salary > then DEPT AVERAGE salary.py
+++ My-Practice/Find Empolyees which salary > then DEPT AVERAGE salary.py
@@ -5,14 +5,27 @@ from pyspark.sql.functions import avg, col
 
 -- identify and display all individual employees whose salary is greater than the average salary of the department they belong to.
 
-# 1. Calculate the average salary per dept (Efficiently, without OrderBy)
-dp_av = employees_df.groupBy('dept_id').agg(avg('salary').alias('dept_avg'))
 
-# 2. Join the calculated average back to the original employee data
-jn_df = employees_df.join(dp_av, on='dept_id', how='inner')
 
-# 3. Filter employees whose salary is greater than the department average
-jn_df.filter(col('salary') > col('dept_avg')).select('emp_id', 'dept_id', 'salary', 'dept_avg').show()
+from pyspark.sql.functions import *
+from pyspark.sql.window import *
+
+
+#identify and display all individual employees whose salary is greater than the average salary of the department they belong to.
+
+
+dpt_av = employees_df.groupBy('dept_id').agg(avg('salary').alias('d_av'))
+
+jn = employees_df.join(dpt_av, 'dept_id')
+
+jn.filter(col('salary') > col('d_av')).show()
+
+
+
+
+
+# 
+
 
 
 
